6f0847e6a870f8391c49a225a6d76943e4e78093
blaqshyd <ayodeletoluloped@gmail.com>
2024-09-09 02:45:08 +0100

    ✨ Feat: Add landing page for docs

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.get("/index.js", (req, res) => res.type('html').send('<h1>Hello</h1>'));
+app.get("/index.js", (req, res) => res.type('html').send(''));
 app.use("/api/users", require("./routes/authRoute"));
 app.listen(port, ()=> console.log(`server running at port ${port}`));
 
